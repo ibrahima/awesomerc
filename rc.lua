c8792822a13e3d94c5346c8c6610ee129d0715f3
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-01-13 16:20:19 -0800

    Switch awesome-gnome to gnome-awesome on desktop
    I should probably just fix wherever I entered this backwards, but whatever.

--- rc.lua
+++ rc.lua
@@ -1,7 +1,7 @@
 -- Override awesome.quit when we're using GNOME
 _awesome_quit = awesome.quit
 awesome.quit = function()
-   if os.getenv("DESKTOP_SESSION") == "awesome-gnome" then
+   if os.getenv("DESKTOP_SESSION") == "gnome-awesome" then
       os.execute("/usr/bin/gnome-session-quit")
    else
       _awesome_quit()
