0adf6fcdd3afe7eabfea38602bd394626507e33c
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-03-17 16:26:24 -0700

    Use different mappings of clients to tags when using multiple monitors.

--- rc.lua
+++ rc.lua
@@ -486,32 +486,65 @@ root.keys(globalkeys)
 -- }}}
 
 -- {{{ Rules
-awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     keys = clientkeys,
-                     buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-    { rule = { class = "mathworks", name = "Figure" },
-      properties = { floating = true } },
-    { rule = { class = "Google-chrome" },
-      properties = { tag = tags[1][2] } },
-    { rule = { class = "Emacs" },
-      properties = { tag = tags[1][1] } },
-    { rule = { class = "Pidgin" },
-      properties = { tag = tags[1][4] } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
-}
+if screen.count() == 1 then
+   awful.rules.rules = {
+      -- All clients will match this rule.
+      { rule = { },
+        properties = { border_width = beautiful.border_width,
+                       border_color = beautiful.border_normal,
+                       focus = awful.client.focus.filter,
+                       keys = clientkeys,
+                       buttons = clientbuttons } },
+      { rule = { class = "MPlayer" },
+        properties = { floating = true } },
+      { rule = { class = "Guake" },
+        properties = { floating = true } },
+      { rule = { class = "pinentry" },
+        properties = { floating = true } },
+      { rule = { class = "gimp" },
+        properties = { floating = true } },
+      { rule = { class = "mathworks", name = "Figure" },
+        properties = { floating = true } },
+      { rule = { class = "Google-chrome" },
+        properties = { tag = tags[1][2] } },
+      { rule = { class = "Emacs" },
+        properties = { tag = tags[1][1] } },
+      { rule = { class = "Pidgin" },
+        properties = { tag = tags[1][4] } },
+      -- Set Firefox to always map on tags number 2 of screen 1.
+      { rule = { class = "Firefox" },
+        properties = { tag = tags[1][2] } },
+   }
+else
+   awful.rules.rules = {
+      -- All clients will match this rule.
+      { rule = { },
+        properties = { border_width = beautiful.border_width,
+                       border_color = beautiful.border_normal,
+                       focus = awful.client.focus.filter,
+                       keys = clientkeys,
+                       buttons = clientbuttons } },
+      { rule = { class = "MPlayer" },
+        properties = { floating = true } },
+      { rule = { class = "Guake" },
+        properties = { floating = true } },
+      { rule = { class = "pinentry" },
+        properties = { floating = true } },
+      { rule = { class = "gimp" },
+        properties = { floating = true } },
+      { rule = { class = "mathworks", name = "Figure" },
+        properties = { floating = true } },
+      { rule = { class = "Google-chrome" },
+        properties = { tag = tags[2][2] } },
+      { rule = { class = "Emacs" },
+        properties = { tag = tags[2][1] } },
+      { rule = { class = "Pidgin" },
+        properties = { tag = tags[1][4] } },
+      { rule = { class = "Firefox" },
+        properties = { tag = tags[2][2] } },
+   }
+
+end
 -- }}}
 
 -- {{{ Signals
