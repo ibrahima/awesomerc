af59421086ae6a27668ed59d61120e6e4dbd68a7
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2017-04-04 13:20:47 -0700

    Add scratchdrop terminal

--- keybindings.lua
+++ keybindings.lua
@@ -1,6 +1,8 @@
-local gears = require("gears");
-local awful = require("awful");
+local gears = require("gears")
+local awful = require("awful")
+local scratchdrop = require("scratchdrop")
 
+local modkey = "Mod4"
 local keybindings = gears.table.join(
    -- Volume keys
    awful.key({ }, "XF86AudioRaiseVolume", function ()
@@ -8,7 +10,10 @@ local keybindings = gears.table.join(
    awful.key({ }, "XF86AudioLowerVolume", function ()
          awful.util.spawn("amixer -c 1 -q set Master 2dB-") end),
    awful.key({ }, "XF86AudioMute", function ()
-         awful.util.spawn("amixer -c 1 -D pulse set Master 1+ toggle") end)
+         awful.util.spawn("amixer -c 1 -D pulse set Master 1+ toggle") end),
+   -- Scratchdrop
+   awful.key({ modkey }, "`", function() scratchdrop("evilvte -g +10+10", "bottom", "center", 1920, 320, false) end)
+
 )
 
 return keybindings
